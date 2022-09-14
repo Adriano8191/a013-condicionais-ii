a1d9fd9bdd9ea3033a27a8fd3e7597e6362e3fa8
adriano <adriano1766@hotmail.com>
2022-09-14 17:16:58 -0300

    Exercicios feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,11 @@
-const num = //seu numero aqui
+const numero = +prompt('Digite um número')
+if(numero % 2 == 0) {
+    console.log('É divisivel por 2')
+}
+if(numero % 3 == 0){
+    console.log('É divisivel por 3')
+} 
 
+if(numero % 2 == 0 || numero % 3 == 0){
+    console.log('É divisivel')
+}
